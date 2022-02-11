788262383f0e466477d4888399dcc79b8c6da00b
ShreyaTheCoder <ranjeetkmishra@gmail.com>
2022-02-11 20:34:34 +0530

    Initial Commit

--- app.js
+++
@@ -1,7 +0,0 @@
-var btnTranslate = document.querySelector("#BTN-Translating");
-
-console.log(btnTranslate)
-
-btnTranslate.addEventListene("click", function clickEventHandler){
-    console.log("clicked")
-}
